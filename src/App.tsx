d4e3a9c58ea4f779a2d8e60232568773dcb49172
swan bil <swanbil@Air-de-mac.lan>
2025-12-29 22:46:10 +0100

    fix: size blurry effect

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <section className="h-[92vh]">
           <div className='relative w-full h-full overflow-hidden'>
 
-            <div className="absolute left-1/2 w-[80%] h-[600px] rounded-b-fullblur-3xl opacity-70" style={{ backgroundColor: "#ffc257" }} id="sun" />
+            <div className="absolute left-1/2 w-[80%] h-[600px] rounded-b-full blur-3xl opacity-70" style={{ backgroundColor: "#ffc257" }} id="sun" />
 
             <div className="absolute bottom-0 left-0 w-full pb-4">
               <div className='relative'>
-                <h1 className="font-semibold tracking-wider text-[270px] leading-none text-center">
+                <h1 className="font-medium tracking-wider text-[270px] leading-none text-center">
                   Swan <span className='font-thin'>Bilek</span>
                 </h1>
               </div>
